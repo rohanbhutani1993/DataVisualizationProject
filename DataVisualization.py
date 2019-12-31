1ea95e575090a9656e78f171a4872b76665d9930
Rohan Bhutani <rohanbhutani1993@gmail.com>
2019-12-31 22:09:19 +0000

    Create DataVisualization.py

---
+++ DataVisualization.py
@@ -0,0 +1,42 @@
+istall.packages("ggplot2")
+
+ggplot(data = pd, aes(x=X6, y = X4)) +  geom_point(aes(color = X1)) +
++     geom_smooth(method ="lm") +
++     coord_cartesian() +
++     scale_color_gradient() +
++     theme_bw()
+
+ ggplot(data = pd, aes(x=X6)) + geom_area(stat = "bin")
+
+ ggplot(data = pd, aes(x=X6)) + geom_density(kernel = "gaussian")
+
+ ggplot(data = pd, aes(x=X6)) + geom_dotplot()
+
+ ggplot(data = pd, aes(x=X6)) + geom_freqpoly()
+
+ ggplot(data = pd, aes(x=X6)) + geom_histogram()
+
+ ggplot(data = pd, aes(x=X6)) + geom_bar()
+
+ ggplot(data = pd, aes(x=X6, y = Y6)) + geom_jitter()
+
+ ggplot(data = pd, aes(x=X6, y = Y6)) + geom_point()
+
+ ggplot(data = pd, aes(x=X6, y = Y6)) + geom_rug()
+
+ ggplot(data = pd, aes(x=X6, y = Y6)) + geom_smooth()
+
+ ggplot(data = pd, aes(x=X6, y = Y6)) + geom_text(aes(label = X6))
+
+ ggplot(data = pd, aes(x=X6, y = Y6)) + geom_bin2d(binwidth = c(5, 0.5))
+
+ ggplot(data = pd, aes(x=X6, y = Y6)) + geom_density2d()
+
+ ggplot(data = pd, aes(x=X6, y = Y6)) + geom_step()
+
+ ggplot(data = pd, aes(x=X6, y = Y6)) + geom_area()
+
+
+ ggplot(data = pd, aes(x=X6, y = Y6)) + geom_line()
+
+
